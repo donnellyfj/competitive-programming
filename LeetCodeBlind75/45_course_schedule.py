17447c47afa2fce283b07060e56376ab47eca17f
donnellyfj <donnellyfj@gmail.com>
2023-09-29 20:17:35 -0400

    Problem 45 completed

---
+++ LeetCodeBlind75/45_course_schedule.py
@@ -0,0 +1,38 @@
+# 207. Course Schedule
+from typing import List
+from collections import defaultdict
+def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    mapping = defaultdict(lambda: [])
+    for course, pre in prerequisites:
+        mapping[course].append(pre)
+
+    # Traverse like a graph, look for cycles
+    visited = [False] * numCourses
+    stack = [False] * numCourses
+    def dfs(course, stack):
+        if stack[course]:
+            return False
+        if visited[course]:
+            return True
+        
+        # Add course to recursion stack and visited
+        stack[course] = True
+        visited[course] = True
+        for pre in mapping[course]:
+            if not visited[pre]:
+                out = dfs(pre, stack)
+                if out == False:
+                    return False
+            if stack[pre]:
+                return False
+        
+        # Remove course from recursion stack
+        stack[course] = False
+        
+        return True
+    
+    for course, pre in prerequisites:
+        if not visited[course] and dfs(course, stack) == False:
+            return False
+
+    return True
