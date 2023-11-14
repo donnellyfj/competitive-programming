cc3ccd014cb48754aa77fe9dec244554c68d1db0
donnellyfj <donnellyfj@gmail.com>
2023-11-14 15:00:57 -0500

    Problem 73 completed

---
+++ LeetCodeBlind75/73_subtree.py
@@ -0,0 +1,20 @@
+# 572. Subtree of Another Tree
+def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            def check(node, subNode):
+                if node == None or subNode == None:
+                    return node == subNode
+                if node.val != subNode.val:
+                    return False
+                return check(node.left, subNode.left) and check(node.right, subNode.right)
+            
+            if node == None:
+                return False
+            # At each node, if the value equals the subRoot value, check the subtree
+            out = False
+            if node.val == subRoot.val:
+                out = check(node, subRoot)
+            
+            return out or dfs(node.left) or dfs(node.right)
+        
+        return dfs(root)
