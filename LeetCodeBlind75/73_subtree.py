b117d0b196e935c358bb45a8486f1d0ae751cec2
donnellyfj <donnellyfj@gmail.com>
2023-11-14 15:04:40 -0500

    Problem 73 completed

--- LeetCodeBlind75/73_subtree.py
+++ LeetCodeBlind75/73_subtree.py
@@ -1,20 +1,21 @@
 # 572. Subtree of Another Tree
 def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def dfs(node):
-            def check(node, subNode):
-                if node == None or subNode == None:
-                    return node == subNode
-                if node.val != subNode.val:
-                    return False
-                return check(node.left, subNode.left) and check(node.right, subNode.right)
-            
-            if node == None:
+    def dfs(node):
+        def check(node, subNode):
+            if node == None or subNode == None:
+                return node == subNode
+            if node.val != subNode.val:
                 return False
-            # At each node, if the value equals the subRoot value, check the subtree
-            out = False
-            if node.val == subRoot.val:
-                out = check(node, subRoot)
-            
-            return out or dfs(node.left) or dfs(node.right)
+            return check(node.left, subNode.left) and check(node.right, subNode.right)
         
-        return dfs(root)
+        if node == None:
+            return False
+        # At each node, if the value equals the subRoot value, check the subtree
+        # This is a similar idea to using a trie to search for a string
+        out = False
+        if node.val == subRoot.val:
+            out = check(node, subRoot)
+        
+        return out or dfs(node.left) or dfs(node.right)
+    
+    return dfs(root)
