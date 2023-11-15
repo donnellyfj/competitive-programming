a35d32864136d54de039de69cf898b68ef77691e
donnellyfj <donnellyfj@gmail.com>
2023-11-14 19:09:47 -0500

    Problem 61 completed

---
+++ LeetCodeBlind75/61_encode_decode.py
@@ -0,0 +1,17 @@
+# 271. Encode and Decode Strings
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        return " ".join(strs)
+
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        return str.split()
