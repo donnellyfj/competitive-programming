3d14e70f21431af032b5de33895f9fbe579635ac
donnellyfj <donnellyfj@gmail.com>
2024-01-29 18:12:15 -0500

    Problem C completed

--- set185/c.cpp
+++ set185/c.cpp
@@ -1,46 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Fun with Even Subarrays
+// Board Moves
 
-void solve(int n, vector<int> arr) {
-    // Intuition: Double the suffix of numbers equal to the last element in arr until the whole
-    //            array equals the last element.
-    int suffixLen = 0;
-
-    // Double the size of the suffix until all elements are equal
-    int out = 0;
-    while (suffixLen < n) {
-        // Find first element from the end of arr not equal to the last element
-        for (int i = n - 1 - suffixLen; i >= 0; i--) {
-            if (arr[i] != arr.back()) {
-                break;
-            }
-            suffixLen++;
-        }
-
-        if (suffixLen < n) {
-            out++;
-        }
-        suffixLen *= 2;
+void solve(long long int n) {
+    // Intuition: Add distances from each odd number up to n.
+    long long int out = 0;
+    while (n > 1) {
+        out += 8 * (n / 2) * (n / 2);
+        n -= 2;
     }
-    
     cout << out << '\n';
 }
 
 int main() {
     int t;
-    int n;
-    vector<int> arr;
+    long long int n;
  
     cin >> t;
     while (t--) {
         cin >> n;
-        arr.resize(n);
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        solve(n, arr);
+        solve(n);
     }
  
     return 0;
